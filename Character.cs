f165db65bcab387c9eff541693e845aedf0c8b48
Mitchell Thomas <mthomas60@my.wctc.edu>
2024-10-01 13:28:33 -0500

    Adds Character class to project

---
+++ Character.cs
@@ -0,0 +1,23 @@
+class Character{
+    public UInt64 Id {get; set;}
+    public String Name { get; set; } = string.Empty;
+    public String Description { get; set; } = string.Empty;
+    public String Appearance { get; set; } = string.Empty;
+    public UInt64 YearCreated { get; set; }
+
+    public Character (UInt64 Id, String Name, String Description, String Appearance, UInt64 YearCreated){
+        this.Id = Id;
+        this.Name = Name;
+        this.Description = Description;
+        this.Appearance = Appearance;
+        this.YearCreated = YearCreated;
+    }
+
+    public String DisplayCharacter(){
+        return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nFirst Appearance: {Appearance}\nYear Created: {YearCreated}";
+    }
+
+    public String GenerateCSV(){
+        return $"{Id},{Name},{Description},{Appearance},{YearCreated}";
+    }
+}
